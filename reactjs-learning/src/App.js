ef671ccb55860d9999dcce3140bba0bb9d6ff595
juni381 <juni381.sj@gmail.com>
2019-09-08 13:44:35 +0500

    nestlings components

--- reactjs-learning/src/App.js
+++ reactjs-learning/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Ninjas from './Ninjas';
 
 function App() {
   return (
     <div className="App">
         <h1>React Learning</h1>
         <h4>My First React App</h4>
+        <Ninjas />
     </div>
   );
 }
---
+++ reactjs-learning/src/Ninjas.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Ninjas extends Component {
+
+    render() {
+        return (
+            <div className='ninja'>
+                <div>Name : Learner</div>
+                <div>Age : 26</div>
+                <div>Hobby : Coding </div>
+            </div>
+        )
+    }
+}
+
+export default Ninjas;
